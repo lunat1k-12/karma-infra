afc4ce06d781a0bd1008b3b5247b5047656badef
yvgenchemeris <chemeris.lunat1k@gmail.com>
2024-09-18 20:01:57 -0700

    Initial commit

---
+++ test/karma-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KarmaInfra from '../lib/karma-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/karma-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KarmaInfra.KarmaInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
