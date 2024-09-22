29f79e03dca78e0185179ad7596aa3b1a1e054d9
yvgenchemeris <chemeris.lunat1k@gmail.com>
2024-09-22 10:39:23 -0700

    Metric updated

--- lib/metric-stack.ts
+++ lib/metric-stack.ts
@@ -1,62 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import {Dashboard, GraphWidget, MathExpression, Metric} from "aws-cdk-lib/aws-cloudwatch";
+import {Dashboard, GraphWidget, MathExpression, GraphWidgetView} from "aws-cdk-lib/aws-cloudwatch";
 import {Duration} from "aws-cdk-lib";
 
 export class MetricStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Local
-    const messagesMetricLocal = new MathExpression({
-      label: "",
-      expression: "SEARCH('{MessagesLocal,Author} MetricName=MessageCount', 'Sum', 300)"
-    });
-
-    const forwardMetricLocal = new MathExpression({
-      label: "",
-      expression: "SEARCH('{ForwardChannelLocal,ChannelName} MetricName=ForwardCount', 'Sum', 300)"
-    });
-
-    new Dashboard(this, "LocalMessagesDashboard", {
-      dashboardName: "LocalMessagesDashboard",
-      defaultInterval: Duration.days(1)
-    }).addWidgets(
-        new GraphWidget({
-            title: "Messages count",
-            width: 12,
-            left: [messagesMetricLocal]
-        }),
-        new GraphWidget({
-            title: "Forward count",
-            width: 12,
-            left: [forwardMetricLocal]
-        })
-    );
-
     // Prod
     const messagesMetric = new MathExpression({
       label: "",
-      expression: "SEARCH('{Messages,Author} MetricName=MessageCount', 'Sum', 300)"
+      expression: "SEARCH('{Messages,Author} MetricName=MessageCount', 'Sum', 86400)"
     });
 
       const forwardMetric = new MathExpression({
           label: "",
-          expression: "SEARCH('{ForwardChannel,ChannelName} MetricName=ForwardCount', 'Sum', 300)"
+          expression: "SEARCH('{ForwardChannel,ChannelName} MetricName=ForwardCount', 'Sum', 2592000)"
       });
 
     new Dashboard(this, "MessagesDashboard", {
       dashboardName: "MessagesDashboard",
-      defaultInterval: Duration.days(1)
+      defaultInterval: Duration.days(7),
     }).addWidgets(
         new GraphWidget({
             title: "Messages count",
             width: 12,
+            period: Duration.days(1),
+            statistic: 'Sum',
             left: [messagesMetric]
         }),
         new GraphWidget({
             title: "Forward count",
             width: 12,
+            period: Duration.days(30),
+            statistic: 'Sum',
+            view: GraphWidgetView.BAR,
             left: [forwardMetric]
         })
     );
